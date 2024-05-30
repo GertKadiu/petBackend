0912f8903202eb350a0af47437adb4197ad10580
GertKadiu <gertikadiu56@gmail.com>
2024-05-30 12:08:32 +0200

    port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
